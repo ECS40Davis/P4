fc04dc7214193c4b85340917e318f9fe6d7aa0e3
MaJessica9 <jamma@ucdavis.edu>
2015-05-03 01:46:49 -0700

    Update plane.cpp

--- plane.cpp
+++ plane.cpp
@@ -1,25 +1,86 @@
-  // Authors: Jessica and Nathaniel
-  #include <iostream>
-  #include <fstream>
-  #include <cstring>
-  #include <cstdlib>
-  #include <cmath>
-  #include "plane.h"
-  using namespace std;
+// Authors: Jessica and Nathaniel
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include "plane.h"
+using namespace std;
   
-  const double Plane::jetFuelPrice = 3.39;
-  
-  double flightTime(const int miles)const; // single trip
-  double fuelCost(const int miles, const int trips)const; // x trips
-  double attendantCost(const int passengers, const double hours; int trips)const; // x trips
-  double pilotCost(const int trips)const; // x trips
-  double maintenanceCost(const int miles, const double hours)const; // x trips
+const double Plane::jetFuelPrice = 3.39;
 
-public:
-  Plane(); // default plane
-  ~Plane(); // destruct plane
-  void readPlane(); // reads in a plane
-  friend ostream & operator << (ostream& os; const Plane &rhs); // displayPlaneInformation()
-  void addPlaneInformation(); // addPlaneInformation()
-  double totalCost(const int passengers, const int miles); // x trips determineBestPlane()
-  const char* getName(const Plane &plane)const; // determineBestPlane()
+Plane::Plane() // default plane
+{
+  name[0] = '\0';
+  passengers = 0;
+  range = 0;
+  speed = 0;
+  fuel = 0;
+  price = 0;
+} // Plane()
+
+
+Plane::~Plane() // destruct plane
+{
+  
+} // ~Plane()
+
+
+void Plane::readPlane() // reads in a plane
+{
+  
+} // readPlane()
+
+
+friend ostream& Plane::operator<< (ostream& os; const Plane &rhs) // displayPlaneInformation()
+{
+  
+} // operator<<
+
+
+void addPlaneInformation() // addPlaneInformation() ???????????????????????????????????????????
+{
+  
+} // addPlaneInformation() 
+
+
+double Plane::totalCost(const int passengers, const int miles) // x trips determineBestPlane()
+{
+  
+} // totalCost()
+
+
+double Plane::flightTime(const int miles)const // single trip
+{
+  
+} // flightTime()
+
+
+double Plane::fuelCost(const int miles, const int trips)const // x trips
+{
+  
+} // fuelCost
+
+
+double Plane::attendantCost(const int passengers, const double hours; int trips)const // x trips
+{
+  
+} // attendantCost()
+
+
+double Plane::pilotCost(const int trips)const // x trips
+{
+  
+} // pilotCost()
+
+
+double Plane::maintenanceCost(const int miles, const double hours)const // x trips
+{
+  
+} // maintenanceCost
+
+
+const char* Plane::getName(const Plane &plane)const // determineBestPlane()
+{
+  
+} // getName()

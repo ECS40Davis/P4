826356b0fe683d375503da96e6aa820feb266f47
MaJessica9 <jamma@ucdavis.edu>
2015-05-03 01:18:15 -0700

    Update plane.h

--- plane.h
+++ plane.h
@@ -6,6 +6,7 @@ using namespace std;
 
 class Plane
 {
+  // planes.dat Format
   char name[12];
   int passengers;
   int range;
@@ -13,29 +14,23 @@ class Plane
   int fuel;
   int price;
   
+  // Calculating Cost
   static const double jetFuelPrice;
   
-  
-  
-  // Calculating Cost
-  double fuelCost(); // x trips
-  double attendantCost(); // x trips
-  double pilotCost(); // x trips
-  double maintenanceCost(); // x trips
-  
-  
+  double flightTime(const int miles)const; // single trip
+  double fuelCost(const int miles, const int trips)const; // x trips
+  double attendantCost(const int passengers, const double hours; int trips)const; // x trips
+  double pilotCost(const int trips)const; // x trips
+  double maintenanceCost(const int miles, const double hours)const; // x trips
 
 public:
   Plane(); // default plane
   ~Plane(); // destruct plane
+  void readPlane(); // reads in a plane
   friend ostream & operator << (ostream& os; const Plane &rhs); // displayPlaneInformation()
   void addPlaneInformation(); // addPlaneInformation()
-  double totalCost(); // x trips determineBestPlane()
-  const char* getName(const Plane plane)const; // determineBestPlane()
-  
-  
+  double totalCost(const int passengers, const int miles); // x trips determineBestPlane()
+  const char* getName(const Plane &plane)const; // determineBestPlane()
 }; // class Plane
-
-
 
 #endif // PLANE_H

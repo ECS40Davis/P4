5dc7a58cc838a357ecef88f50f74da86f08b77cd
MaJessica9 <jamma@ucdavis.edu>
2015-05-03 20:22:36 -0700

    Update plane.h

--- plane.h
+++ plane.h
@@ -21,20 +21,21 @@ private:
   double calcFlightTime(const int miles)const; // single trip
   int calcNumTrips(const int miles); // number of trips
   double calcFuelCost(const int miles, const int trips)const; // x trips
-  int calcNumAttendants(); // single trip
-  double calcAttendantCost(const int passengers, const double hours; int trips)const; // x trips
-  int calcNumPilots(); // single trip
-  double calcPilotCost(const int trips)const; // x trips
-  double calcMaintenanceCost(const int miles, const double hours)const; // x trips
+  int calcNumAttendants(void)const; // single trip
+  double calcAttendantCost(const int hours, int trips)const; // x trips
+  int calcNumPilots(void)const; // single trip
+  double calcPilotCost(const int hours, const int trips)const; // x trips
+  double calcMaintenanceCost(const double hours, const int trips)const; // x trips
 
 public:
   Plane(); // default plane
   ~Plane(); // destruct plane
   void readPlane(); // reads in a plane
-  friend ostream & operator << (ostream& os; const Plane &rhs); // displayPlaneInformation()
+  friend ostream & operator << (ostream& os, const Plane &rhs); // displayPlaneInformation()
   void addPlaneInformation(); // addPlaneInformation()
   double calcTotalCost(const int passengers, const int miles); // x trips determineBestPlane()
   const char* getName(const Plane &plane)const; // determineBestPlane()
 }; // class Plane
 
 #endif // PLANE_H
+

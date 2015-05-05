b1bb03c702f81b22920b35b96c7a04947475e5ad
MaJessica9 <jamma@ucdavis.edu>
2015-05-04 19:21:16 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -3,22 +3,33 @@
 // Author Sean Davis
 
 #include "city.h"
+#include "plane.h"
 
 class Vector
 {
+  // City Declaration
   City *cityArray;
   int size;
   int count;
   void resize();
+  
+  // Plane Declaration
+  Plane *planeArray;
+  
 public:
   Vector();
   ~Vector();
+  // For City
   void calcAirportTraffic(int index) const;
   void calcDistance(int index1, int index2) const;
   void cleanCities();
   int findAirport(const char *airport) const;
   void readAirports();
   void readCities();
+  
+  // For Plane
+  int readPlanes();
 };  // class Vector
 
 #endif	// VECTOR_H 
+

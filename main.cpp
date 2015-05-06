15b32a6c21e12c83b486666825fbc85377b4b4fa
MaJessica9 <jamma@ucdavis.edu>
2015-05-06 01:34:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <iomanip>
 #include "vector.h"
+#include "plane.h"
 using namespace std;
 
 
@@ -39,21 +41,56 @@ void determineAirportTraffic(const Vector *cities)
 }  // determinAirportTraffic()
 
 
-void displayPlaneInformation(const Plane *planes)
+void displayPlaneInformation(const Plane planes[10], const int numPlanes)
 {
-    
+   cout << "\nPlane Information" << endl; 
+   cout << setw(13) << left << "Name"
+            << setw(6) << "Pass." 
+            << setw(6) << "Range"
+            << setw(6) << "Speed"
+            << setw(7) << "Fuel"
+            << setw(6) << "MPG"
+            << setw(6) << "$/mi"
+            << setw(6) << "Price * 10^6"
+            << endl;
+  for(int i = 0; i < numPlanes; i++)
+    cout << planes[i];
 } // displayPlaneInformation
 
 
-void addPlaneInformation(const Plane *planes)
+void addPlaneInformation(Plane planes[10], int numPlanes)
 {
-
+    numPlanes++;
+    // add cin stuff...
+    planes->addPlaneInformation(); //()<-cin stuff
 } // addPlaneInformation()
 
 
-void determineBestPlane(const Plane *planes)
+void determineBestPlane(const Vector *cities, Plane planes[10], const int numPlanes)
 {
-
+  int minCostPlane = 0;
+  int minPlaneElement = 0;
+  char airport1[80], airport2[80];
+  cout << "\nPlease enter two airport abbreviations (XXX XXX): ";
+  cin >> airport1 >> airport2;
+  
+  int index1 = cities->findAirport(airport1);
+  int index2 = cities->findAirport(airport2);
+  
+  int passengers = cities->calcPassengers(index1, index2);
+  int miles = cities->calcMiles(index1, index2);
+  
+  for (int i = 0; i < numPlanes; i ++)
+  {
+      double j = planes[i].calcTotalCost(passengers, miles);
+      if (j < minCostPlane)
+      {
+          minPlaneElement = i;
+      }// if it is min
+      
+  } // check all planes for their min value
+  
+  planes[minCostPlane].getName();
 } // determineBestPlane())
 
 
@@ -66,26 +103,34 @@ int getChoice()
     cout << "\nFlight Simulator Menu\n";
     cout << "0. Done.\n";
     cout << "1. Determine distance and passengers between two airports.\n";
-    cout << "2. Determine all traffic from one airport.\n\n";
-    cout << "Your choice (0 - 2): ";
+    cout << "2. Determine all traffic from one airport.\n";
+    cout << "3. Display planes information.\n";
+    cout << "4. Add plan information.\n";
+    cout << "5. Determine best plan between two airports.\n\n";
+    cout << "Your choice (0 - 5): ";
     cin >> choice;
     cin.ignore(1000, '\n');
    
-    if  (choice < 0 || choice > 2)
-      cout << "Your choice must be between 0 and 2. Please try again.\n";
-  } while(choice < 0 || choice > 2);
+    if  (choice < 0 || choice > 5)
+      cout << "Your choice must be between 0 and 5. Please try again.\n";
+  } while(choice < 0 || choice > 5);
    
   return choice;
 }  // getChoice()  
 
 
-void run(const Vector *cities, const Plane *planes)
+void run(const Vector *cities, Plane planes[10], int numPlanes)
 {
   int choice;
+  ifstream inf;
+  inf.open("planes.dat", ios::in | ios::binary);
+  while(!inf.eof())
+  {
+    planes[numPlanes++].readPlane(inf);
+  } // while more in file
   
-//  for(index1 = 0; index1 < cities->count; index1++)
-//    for(index2 = index1 + 1; index2  < cities->count; index2++)
-//      calcDistance(cities, index1, index2);
+  inf.close();
+  numPlanes--;  
 
   do
   {
@@ -98,28 +143,36 @@ void run(const Vector *cities, const Plane *planes)
         determineAirportTraffic(cities);
       else // Done or determine plane information
         if (choice == 3)
-          displayPlaneInformation(planes);
+          displayPlaneInformation(planes, numPlanes);
         else // Done or add plane info
           if (choice == 4)
-            addPlaneInformation(planes);
+          {
+            addPlaneInformation(planes, numPlanes);
+            numPlanes++;
+          }
           else // Done or determine best plane
             if (choice == 5)
-              determineBestPlane(planes);
+              determineBestPlane(cities, planes, numPlanes);
   } while(choice != 0);
 }  // run())
 
 
 int main(int argc, char** argv) 
 {
+  // Cities
   Vector cities;
-  Plane planes;
   cities.readCities();
   cities.readAirports();
   cities.cleanCities();
+  
+  // Planes
+  Plane planes[10];
+  int numPlanes = 0;
+  
 //  srand(1);
-//  for(int i = 0; i < cities.count; i++)
-//    cout << "1\n" << cities.cityArray[i].airport << ' ' << cities.cityArray[rand() % cities.count].airport << endl;
-//  printf("%s %s %lf %lf\n", cities.cityArray[i].name, cities.cityArray[i].airport, cities.cityArray[i].latitude, cities.cityArray[i].longitude);
-  run(&cities, &planes);
+//  for(int i = 0; i < citiesPlanes.count; i++)
+//    cout << "1\n" << citiesPlanes.cityArray[i].airport << ' ' << citiesPlanes.cityArray[rand() % citiesPlanes.count].airport << endl;
+//  printf("%s %s %lf %lf\n", citiesPlanes.cityArray[i].name, citiesPlanes.cityArray[i].airport, citiesPlanes.cityArray[i].latitude, citiesPlanes.cityArray[i].longitude);
+  run(&cities, planes, numPlanes);
   return 0;
 } // main())
